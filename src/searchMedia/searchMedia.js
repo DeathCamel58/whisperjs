f7b04f6065598dcf25ccf07f049127edb0821bb1
Dylan Corrales <deathcamel58@gmail.com>
2023-08-23 09:53:06 -0400

    SearchMedia: Remove bad API call

--- src/searchMedia/searchMedia.js
+++ src/searchMedia/searchMedia.js
@@ -11,7 +11,7 @@ class SearchMedia {
      * @param wid The parent Whisper ID
      * @returns {Promise<*>}
      */
-    async get(query, wid = null) {
+    async suggest(query, wid = null) {
         let base64Query = (new Buffer(query).toString('base64'));
         console.log(`Base64 Encoding:\n\tQuery\t${query}\n\tBase64\t${base64Query}`);
         let params = {
@@ -22,24 +22,6 @@ class SearchMedia {
             query: base64Query + '\n'
         };
         let response = await apiClient('android', '/search_media', 'get', params, null, null, this.user, false);
-        return response;
-    }
-
-    /**
-     * Runs a search for feeds
-     * @param query What to search for
-     * @param wid The parent Whisper ID
-     * @returns {Promise<*>}
-     */
-    async suggest(query, wid = null) {
-        let params = {
-            media_types: 'image,video',
-            uid: this.user.uid,
-            locale: 'en_us',
-            query: query,
-            parent_wid: wid
-        };
-        let response = await apiClient('android', '/search/suggest/', 'get', params, null, null, this.user, false);
         return response;
     }
 }
