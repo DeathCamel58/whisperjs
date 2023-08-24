a25437170f51e2ff2abb8f4fb9a3a41ebd93ba3a
Dylan Corrales <deathcamel58@gmail.com>
2023-08-24 11:36:43 -0400

    Share: Use axios calls

--- src/share/share.js
+++ src/share/share.js
@@ -1,4 +1,4 @@
-const {apiClient} = require("../util/apiClient");
+const axios = require("axios");
 
 class Share {
     constructor(user) {
@@ -10,16 +10,24 @@ class Share {
      * @returns {Promise<*>}
      */
     async link(feed_id) {
-        let params = {
-            uid: this.user.uid,
-            locale: 'en_us',
-            id: feed_id,
-            type: 'feed',
-            link_type: 'invite',
-            channel: 'copy'
-        };
-        let response = await apiClient('android', '/share/link', 'get', params, null, null, this.user, false);
-        return response;
+        let config = {
+            headers: {
+                Publisher_version: 'android_9.68.0',
+                Session_token: this.user.sessionToken,
+                'Content-Type': 'x-www-form-urlencoded'
+            },
+            params: {
+                uid: this.user.uid,
+                id: feed_id,
+                type: 'feed',
+                link_type: 'invite',
+                channel: 'copy'
+            }
+        }
+
+        let response = await axios.get('https://prod-android.whisper.sh/share/link', config);
+
+        return response.data;
     }
 }
 
